881881051d8c9b129a79f4d1cae1d6b92bd315fa
Diogo Cardoso <diogocardoso92@outlook.com>
2019-01-28 23:33:48 +0000

    Update blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Diogo Cardoso | Blog',
+    title: 'Diogo Cardoso - Blog',
     description: 'Random stuff. Mostly programming and electronics.',
     author: 'Diogo Cardoso',
   },
