7803ed890dc60a45f952c62471a4a57a84a89cf4
Diogo Cardoso <diogocardoso92@outlook.com>
2019-01-28 23:34:01 +0000

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,15 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout pageTitle="Oups, Somethig went wrong..">
+  <Layout>
     <SEO title="404: Not found" />
+    <div id="notfound">
+      <div class="notfound">
+        <div class="notfound-404">
+          <h1>Oops!</h1>
+        </div>
+      </div>
+    </div>
     <Link className="btn btn-primary text-uppercase" to={'/'}>
       Go home
     </Link>
