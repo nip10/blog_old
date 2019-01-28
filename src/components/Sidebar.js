bc42ddd5e11c222397c2424bc720ec7045510409
Diogo Cardoso <diogocardoso92@outlook.com>
2019-01-28 23:34:30 +0000

    Remove ad from sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -56,12 +56,6 @@ const Sidebar = ({ author, authorFluid }) => (
     </Card>
     <Card>
       <CardBody>
-        <CardTitle className="text-center text-uppercase">Advertisement</CardTitle>
-        <img src="https://via.placeholder.com/320x200" alt="Advert" style={{ width: '100%' }} />
-      </CardBody>
-    </Card>
-    <Card>
-      <CardBody>
         <CardTitle className="text-center text-uppercase mb-3">Recent Posts</CardTitle>
         <StaticQuery
           query={sidebarQuery}
